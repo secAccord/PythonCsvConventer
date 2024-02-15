c5ad67d8cf94a9952bab2b9c0fe9f283ec979da0
Dawid Zgoda <acc0rd.2goda@gmail.com>
2024-02-15 18:52:09 +0100

    Date Fix

--- scripts/ing.py
+++ scripts/ing.py
@@ -26,13 +26,14 @@ def interpretation(file_name):
                 windraw = num
                 num = 0
 
-            date = temp_tab[1].split(".")
+            date = temp_tab[1].split("-")
             okres = date[2] + "." + date[1]
             new_date = date[2] + "-" + date[1] + "-" + date[0]
 
             temp_tab = [okres, new_date, new_date, temp_tab[3], temp_tab[4], "", "", "\"ING PLN\"", windraw, num]
             tab_lines.append(addnow.tab_to_string(temp_tab))
     now = datetime.datetime.now()
+    tab_lines.reverse()
     name = f"{name}_{now.strftime('%Y-%m-%d_%H-%M-%S')}"
     addnow.recreate_csv(tab_lines, f"{name}.csv")
     return
